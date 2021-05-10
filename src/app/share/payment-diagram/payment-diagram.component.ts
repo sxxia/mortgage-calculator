f83d7a5479ce9b3a3b4c9085b2e0c4fbd2a7e9bc
Shuang Xia <shuangx@stradigi.ai>
2021-05-10 08:06:24 -0400

    implement diagram component

--- src/app/share/payment-diagram/payment-diagram.component.ts
+++ src/app/share/payment-diagram/payment-diagram.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-payment-diagram',
   templateUrl: './payment-diagram.component.html',
-  styleUrls: ['./payment-diagram.component.scss']
+  styleUrls: ['./payment-diagram.component.scss'],
 })
-export class PaymentDiagramComponent implements OnInit {
+export class PaymentDiagramComponent implements OnInit, OnChanges {
+  @Input() results: any;
+  areaChart: any;
+  barChart: any;
+  mortgageAmount: number;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngOnChanges(): void {
+    if (this.results) {
+      this.mortgageAmount = this.results.amortization.principalPayment;
+
+      this.areaChart = [
+        {
+          numberOfPayment: 0,
+          remindedAmount: this.mortgageAmount,
+        },
+        {
+          numberOfPayment: this.results.term.numberOfPayment,
+          remindedAmount:
+            this.mortgageAmount - this.results.term.principalPayment,
+        },
+        {
+          numberOfPayment: this.results.amortization.numberOfPayment,
+          remindedAmount: 0,
+        },
+      ];
+
+      this.barChart = [
+        {
+          payments: 'Regulat Payments',
+          interestPayment: this.results.amortization.interestPayment,
+          principalPayment: this.mortgageAmount,
+        },
+      ];
+    }
   }
-
 }
