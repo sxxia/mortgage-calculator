31ca9b17bb430ad23ddf3e26c7bc86a91c2b4c61
Shuang Xia <shuangx@stradigi.ai>
2021-05-10 07:30:34 -0400

    add devextreme

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
 import { CalculationSummaryComponent } from './calculation-summary/calculation-summary.component';
 import { MortgageSummaryComponent } from './mortgage-summary/mortgage-summary.component';
+import { DevExtremeModule } from './dev-extreme/dev-extreme.module';
 
 @NgModule({
   declarations: [CalculationSummaryComponent, MortgageSummaryComponent],
-  imports: [CommonModule, AngularMaterialModule],
+  imports: [CommonModule, AngularMaterialModule, DevExtremeModule],
   exports: [
     AngularMaterialModule,
     CalculationSummaryComponent,
     MortgageSummaryComponent,
+    DevExtremeModule,
   ],
 })
 export class ShareModule {
