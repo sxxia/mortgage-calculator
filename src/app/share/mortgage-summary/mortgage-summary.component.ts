48135c88c4823a5ee4c26b9a03723d7ed2e78af7
Shuang Xia <shuangx@stradigi.ai>
2021-05-09 23:40:54 -0400

    create mortgage summary component

---
+++ src/app/share/mortgage-summary/mortgage-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mortgage-summary',
+  templateUrl: './mortgage-summary.component.html',
+  styleUrls: ['./mortgage-summary.component.scss']
+})
+export class MortgageSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -2,11 +2,16 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
 import { CalculationSummaryComponent } from './calculation-summary/calculation-summary.component';
+import { MortgageSummaryComponent } from './mortgage-summary/mortgage-summary.component';
 
 @NgModule({
-  declarations: [CalculationSummaryComponent],
+  declarations: [CalculationSummaryComponent, MortgageSummaryComponent],
   imports: [CommonModule, AngularMaterialModule],
-  exports: [AngularMaterialModule, CalculationSummaryComponent],
+  exports: [
+    AngularMaterialModule,
+    CalculationSummaryComponent,
+    MortgageSummaryComponent,
+  ],
 })
 export class ShareModule {
   constructor(
