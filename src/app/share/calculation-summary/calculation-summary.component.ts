89a0864e7aaf75363a0243833fb70d1e7098dc12
Shuang Xia <shuangx@stradigi.ai>
2021-05-09 23:12:30 -0400

    add calculation summary component

---
+++ src/app/share/calculation-summary/calculation-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculation-summary',
+  templateUrl: './calculation-summary.component.html',
+  styleUrls: ['./calculation-summary.component.scss']
+})
+export class CalculationSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -1,11 +1,12 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { CalculationSummaryComponent } from './calculation-summary/calculation-summary.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CalculationSummaryComponent],
   imports: [CommonModule, AngularMaterialModule],
-  exports: [AngularMaterialModule],
+  exports: [AngularMaterialModule, CalculationSummaryComponent],
 })
 export class ShareModule {
   constructor(
